23983967e20b8b8414de9d6fd59fd7c2e24b13ae
Rajab Natshah <rajabn@gmail.com>
2020-02-27 10:52:37 +0200

    Issue #3113193: Drupal 9 Readiness for [Vartheme (Bootstrap 3 - LESS)] with Drupal coding standard and practice

--- theme-settings.php
+++ theme-settings.php
@@ -9,6 +9,7 @@
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Implements hook_form_FORM_ID_alter().
@@ -40,7 +41,7 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
     '#type' => 'checkbox',
     '#title' => t('Header Fluid Container'),
     '#description' => t('Use <code>.container-fluid</code> class instead of <code>.container</code> for the Header region.<br />See: @vartheme_link', [
-      '@vartheme_link' => Drupal::l('Fluid container', Url::fromUri('http://getbootstrap.com/css/', ['absolute' => TRUE, 'fragment' => 'grid-example-fluid'])),
+      '@vartheme_link' => Link::fromTextAndUrl('Fluid container', Url::fromUri('http://getbootstrap.com/css/', ['absolute' => TRUE, 'fragment' => 'grid-example-fluid'])),
     ]),
     '#default_value' => theme_get_setting('header_container'),
   ];
@@ -49,9 +50,9 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
   $form['email_logo'] = [
     '#type'     => 'details',
     '#title'    => t('Email Logo'),
-    '#open' => false,
+    '#open' => FALSE,
   ];
-  
+
   $form['email_logo']['email_logo_default'] = [
     "#type" => "checkbox",
     '#title'    => t('Use the logo supplied by the theme'),
@@ -64,16 +65,16 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
       "invisible" => [
         'input[name="email_logo_default"]' => [
           "checked" => TRUE,
-        ]
-      ]
-    ]
+        ],
+      ],
+    ],
   ];
 
   $form['email_logo']['email_logo_settings']["email_logo_path"] = [
     "#type" => "textfield",
     "#title" => "Path to custom logo",
     "#default_value" => theme_get_setting('email_logo_path'),
-    "#description" => t("Examples: <code>@external-file</code>", ["@external-file"=> "http://www.example.com/logo.png"])
+    "#description" => t("Examples: <code>@external-file</code>", ["@external-file" => "http://www.example.com/logo.png"]),
   ];
 
   $form['email_logo']['email_logo_settings']["email_logo_upload"] = [
@@ -81,7 +82,7 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
     "#title"    => t("Upload logo image"),
     "#description" => t("If you don't have direct file access to the server, use this field to upload your logo."),
     '#required' => FALSE,
-    '#upload_location' => file_default_scheme() . '://theme/email_logo/',
+    '#upload_location' => \Drupal::config('system.file')->get('default_scheme') . '://theme/email_logo/',
     '#default_value' => theme_get_setting('email_logo_upload'),
     '#upload_validators' => [
       'file_validate_extensions' => ['gif png jpg jpeg'],
